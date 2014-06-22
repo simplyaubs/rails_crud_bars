e4ffe6f72768c0a221e416f817a6833c53bbd5bc
Aubrey Howell <simplyaubs@gmail.com>
2014-06-22 19:02:56 -0400

    User can update a bar from list.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -15,6 +15,21 @@ class BarsController < ApplicationController
     end
   end
 
+  def show
+    @bar = Bar.find(params[:id])
+  end
+
+  def edit
+    @bar = Bar.find(params[:id])
+  end
+
+  def update
+    @bar = Bar.find(params[:id])
+    @bar.update_attributes!(bar_params)
+
+    redirect_to bars_path
+  end
+
   private
   def bar_params
     params.require(:bar).permit(:name, :drink)
